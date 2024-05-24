b48160d5c94454942e103693f1ccd5fa100fb249
Ernest Sarfo <ernestsarfo100@gmail.com>
2024-05-24 13:49:29 +0000

    initial commit

---
+++ src/test/java/dev/thesarfo/push_notif/PushNotifApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.thesarfo.push_notif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotifApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
